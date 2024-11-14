8ad9c39269dd84b30e1d494af5e260b5e30a5581
David Norman <norma240@umn.edu>
2024-11-14 08:15:26 -0600

    Time examples

--- hello_app.py
+++ hello_app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import time
+from datetime import datetime
+from datetime import timezone
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>goodbye,world!</p>"
+    DAYNAMES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                "Saturday", "Sunday"]
+    dayname = DAYNAMES[datetime.now().weekday()]
+    day = datetime.now(timezone.utc)
+    epoch = time.time()
+    return f"<p>Goodbye,world! Happy {dayname}, today is {day} or epoch time:{epoch}.</p>"
